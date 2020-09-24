ff79962235b3627e1ca526122570f6e448f22693
Maxim Bezrukavnikov <60591825+m-bezrukavnikov-hotlead-io@users.noreply.github.com>
2020-09-24 17:25:34 +1000

    Update refresher.user.js

--- refresher.user.js
+++ refresher.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Refresher
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       You
 // @match        https://crm.hotlead.io/callcenter
